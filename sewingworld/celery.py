52aceafdc79d1121a9c66caf742a92040207be4f
Andrey Novikov <novikov@gmail.com>
2018-09-25 21:38:35 +0300

    Singer amendments

--- sewingworld/celery.py
+++ sewingworld/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sewingworld.settings.janome')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sewingworld.settings.singer')
 
 app = Celery('sewingworld')
 
