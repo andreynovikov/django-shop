60f956541c0518cb252236a157330df9230e3d1d
Andrey Novikov <novikov@gmail.com>
2018-11-29 19:36:13 +0300

    User update form enhancements

--- shop/forms.py
+++ shop/forms.py
@@ -1,6 +1,7 @@
 import os
 import re
 from django import forms
+from django.forms.models import model_to_dict
 from suit.widgets import AutosizedTextarea
 from django.conf import settings
 
@@ -16,6 +17,19 @@ class UserForm(forms.Form):
     phone = forms.CharField(label='Телефон', max_length=30, help_text='Мы принимаем только мобильные телефоны')
     email = forms.EmailField(label='Эл.почта', required=False)
     address = forms.CharField(label='Адрес', max_length=255, required=False)
+    username = forms.CharField(label='Псевдоним', max_length=100, required=False, help_text='Отображается в форуме')
+
+    def __init__(self, *args, **kwargs):
+        self.instance = kwargs.pop('user', None)
+        if self.instance:
+            kwargs['initial'] = model_to_dict(self.instance)
+        super(UserForm, self).__init__(*args, **kwargs)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if ShopUser.objects.filter(username=username).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError("Такой псевдоним уже используется")
+        return username
 
 
 class OneSImportForm(forms.Form):
