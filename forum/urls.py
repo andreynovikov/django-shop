9cd90c80c1ef7c8336591adda2f1d521c8564589
Andrey Novikov <novikov@gmail.com>
2018-12-25 00:16:47 +0300

    Old forum navigation tree

--- forum/urls.py
+++ forum/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex /
+    url(r'^$', views.index, name='index'),
+    # ex: /topic/8/
+    url(r'^topic/(?P<id>\d+)/$', views.topic, name='topic'),
     # ex: /thread/8/
     url(r'^thread/(?P<id>\d+)/$', views.thread, name='thread'),
 ]
