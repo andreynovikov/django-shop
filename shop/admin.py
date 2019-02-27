50981daeea0824d5c18cdbbf4a1f242957f9e66a
Andrey Novikov <novikov@gmail.com>
2019-02-27 17:04:37 +0300

    Recalculate wholesale stock

--- shop/admin.py
+++ shop/admin.py
@@ -109,7 +109,7 @@ class CategoryAdmin(DraggableMPTTAdmin):
 
 @admin.register(Supplier)
 class SupplierAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ['id', 'code', 'name', 'show_in_order', 'count_in_stock', 'spb_count_in_stock']
+    list_display = ['code', 'name', 'show_in_order', 'count_in_stock', 'spb_count_in_stock', 'ws_count_in_stock']
     list_display_links = ['name']
     search_fields = ['code', 'name']
 
--- shop/signals.py
+++ shop/signals.py
@@ -16,6 +16,7 @@ def order_saved(sender, **kwargs):
     for item in order.items.all():
         item.product.num = -1
         item.product.spb_num = -1
+        item.product.ws_num = -1
         item.product.save()
 
     if order.tracker.has_changed('status'):
