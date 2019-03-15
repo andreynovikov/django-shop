5c877bcf9715f828dc3241b10c1d2d933875e04d
Andrey Novikov <novikov@gmail.com>
2019-03-16 00:24:09 +0300

    Local amendments

--- sewingworld/urls.py
+++ sewingworld/urls.py
@@ -7,6 +7,7 @@ import mptt_urls
 
 from . import views
 
+
 urlpatterns = [
     # ex: /
     url(r'^$', views.index, name='index'),
@@ -16,8 +17,6 @@ urlpatterns = [
     url(r'^products\.xml$', views.products, {'template': 'products.xml'}, name='products'),
     # ex: /google.xml
     url(r'^google\.xml$', views.products, {'template': 'google.xml'}, name='google'),
-    # ex: /cc-prym.xml
-    url(r'^cc-prym\.xml$', views.products, {'template': 'prym.xml'}, name='cc-prym'),
     # ex: /search/
     url(r'^search/$', views.search, name='search'),
     # ex: /catalog/
