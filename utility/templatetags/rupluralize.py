037a013804be6edfac5fa8524cc1174e511ecfa8
Andrey Novikov <novikov@gmail.com>
2019-07-07 21:33:22 +0300

    Template filters

--- utility/templatetags/rupluralize.py
+++ utility/templatetags/rupluralize.py
@@ -8,11 +8,17 @@ register=template.Library()
 # {{someval|rupluralize:"товар,товара,товаров"}}
 @register.filter(is_safe = False)
 @stringfilter
-def rupluralize(value, arg):
-    bits = arg.split(u',')
+def rupluralize(value, endings):
     try:
-        value = str( 0 if not value or int(value) <= 0 else value )[-1:]
-        return bits[ 0 if value=='1' else (1 if value in '234' else 2) ]
-    except:
-        raise template.TemplateSyntaxError
-    return ''
+        endings = endings.split(',')
+        value = int(value)
+        if value % 100 in (11, 12, 13, 14):
+            return endings[2]
+        if value % 10 == 1:
+            return endings[0]
+        if value % 10 in (2, 3, 4):
+            return endings[1]
+        else:
+            return endings[2]
+    except Exception as e:
+        raise template.TemplateSyntaxError(str(e))
