25b706d0a31fba9cd7c0838aa2bca3db048ed2f0
Andrey Novikov <novikov@gmail.com>
2019-08-22 16:32:47 +0300

    More dynamic settings

--- sewingworld/forms.py
+++ sewingworld/forms.py
@@ -5,3 +5,7 @@ from djconfig.forms import ConfigForm
 
 class SWConfigForm(ConfigForm):
     sw_sms_provider = forms.ChoiceField(label='Смс провайдер', initial='sms_uslugi', choices=[('sms_uslugi', 'смс-услуги'), ('smsru', 'sms.ru')])
+
+    sw_email_from = forms.CharField(label='Адрес эл.почты робота')
+    sw_email_unisender = forms.CharField(label='Адрес эл.почты Unisender')
+    sw_email_managers = forms.CharField(label='Адрес эл.почты менеджера заказов', help_text='Можно несколько через запятую')
