a947deb133e11f276400b4b2dc1d51ae3eab7793
Andrey Novikov <novikov@gmail.com>
2020-02-05 13:27:58 +0300

    admin: fix concurrent order updates

--- shop/views.py
+++ shop/views.py
@@ -10,7 +10,6 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpRe
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.sites.models import Site
 from django_ipgeobase.models import IPGeoBase
 from django.template.loader import render_to_string
 from django.views.decorators.http import require_POST
