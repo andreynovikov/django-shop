faf60c59896d154d522bb228e3dee5c7b8958514
Andrey Novikov <novikov@gmail.com>
2022-03-27 14:07:16 +0300

    Universal integrations, second step

--- shop/tasks.py
+++ shop/tasks.py
@@ -39,7 +39,7 @@ def validate_email(email):
 
 
 def get_site_for_order(order):
-    if order.is_beru or order.is_from_market:
+    if order.is_integration:
         return sw_default_site
     else:
         return order.site
