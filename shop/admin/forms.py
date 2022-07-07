c177512d6da17365c3cf15b5e27a19ff87e9e1cf
Andrey Novikov <novikov@gmail.com>
2022-07-07 10:47:14 +0300

    admin: Ozon integration, first step

--- shop/admin/forms.py
+++ shop/admin/forms.py
@@ -324,10 +324,12 @@ class OrderAdminForm(forms.ModelForm):
             self.fields['user_tags'].widget = TagAutoComplete(model=type(instance.user), attrs=self.fields['user_tags'].widget.attrs)
 
             if instance.integration and instance.integration.settings:
+                utm_source = instance.integration.utm_source
                 ym_campaign = instance.integration.settings.get('ym_campaign', '')
             else:
+                utm_source = ''
                 ym_campaign = ''
-            self.fields['delivery_tracking_number'].widget = DeliveryTrackingNumberWidget(instance.id, ym_campaign)
+            self.fields['delivery_tracking_number'].widget = DeliveryTrackingNumberWidget(instance.id, utm_source, ym_campaign)
             self.fields['delivery_yd_order'].widget = YandexDeliveryWidget(instance.id, config.sw_yd_campaign)
         except (KeyError, AttributeError):
             pass
