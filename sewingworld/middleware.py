15d2dfaa131ebfa6da5ebe330cbafbb12286b5d7
Andrey Novikov <novikov@gmail.com>
2022-11-25 23:11:06 +0300

    SiteDetectionMiddleware for Rest API

--- sewingworld/middleware.py
+++ sewingworld/middleware.py
@@ -1,5 +1,44 @@
-import traceback
+import logging
 import sys
+import traceback
+
+from urllib.parse import urlparse
+
+from django.contrib.sites.models import Site
+from django.db.models import Q
+
+
+logger = logging.getLogger("django")
+
+
+class SiteDetectionMiddleware:
+    """
+    This logic is used in Rest API
+    TODO: accommodate it to general use
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        logger.error("Origin: " + request.META.get('HTTP_ORIGIN', ''))
+        logger.error("Referer: " + request.META.get('HTTP_REFERER', ''))
+        site = None
+        # Prefer Origin header over Referer
+        url = request.META.get('HTTP_ORIGIN', request.META.get('HTTP_REFERER', ''))
+        if url != '':
+            try:
+                domain = urlparse(url).hostname
+                if domain:
+                    site = Site.objects.filter(Q(domain=domain) | Q(profile__aliases__contains=[domain])).first()
+            except ValueError:
+                pass
+        if site is None:
+            site = Site.objects.get_current()
+        request.site = site
+        logger.error(site)
+        response = self.get_response(request)
+        return response
 
 
 class ProcessExceptionMiddleware(object):
