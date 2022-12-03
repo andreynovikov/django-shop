16ec8d7aa2e73ead3e9125c80002e59f6f52b788
Andrey Novikov <novikov@gmail.com>
2022-12-03 14:42:23 +0300

    Standalone deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     reactStrictMode: true,
     trailingSlash: true,
+    output: 'standalone',
     distDir: process.env.BUILD_DIR || '.next',
     sassOptions: {
         includePaths: [path.join(__dirname, 'src/vendor/sell/scss')],
