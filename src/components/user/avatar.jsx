3c384e244db3aaf69859248918d110c38c634b57
Andrey Novikov <novikov@gmail.com>
2022-12-05 11:47:14 +0300

    Do not try gravatar if key is not supplied

--- src/components/user/avatar.jsx
+++ src/components/user/avatar.jsx
@@ -6,27 +6,35 @@ export default function UserAvatar({gravatar, name, size, border, className}) {
     const [avatar, setAvatar] = useState('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=');
 
     useEffect(() => {
-        const d = name.startsWith('+7') ? 'mp' : '404';
-        axios.get(gravatar + '&d=' + d, {
-            responseType: 'arraybuffer'
-        }).then((response) => {
-            // const buffer = URL.createObjectURL(response.data); - with responseType: 'blob'
-            const buffer = Buffer.from(response.data, 'binary').toString('base64');
-            setAvatar(`data:${response.headers['content-type'].toLowerCase()};base64,${buffer}`);
-        }).catch((error) => {
-            axios.get('https://ui-avatars.com/api/', {
-                params: {
-                    size: size,
-                    bold: 'true',
-                    name: name
-                },
+        if (gravatar !== '') {
+            const d = name.startsWith('+7') ? 'mp' : '404';
+            axios.get(gravatar + '&d=' + d, {
                 responseType: 'arraybuffer'
             }).then((response) => {
+                // const buffer = URL.createObjectURL(response.data); - with responseType: 'blob'
                 const buffer = Buffer.from(response.data, 'binary').toString('base64');
                 setAvatar(`data:${response.headers['content-type'].toLowerCase()};base64,${buffer}`);
+            }).catch(() => {
+                getUIAvatar(name, size);
             });
-        });
+        } else {
+            getUIAvatar(name, size);
+        }
     }, [gravatar, name, size]);
+
+    const getUIAvatar = (name, size) => {
+        axios.get('https://ui-avatars.com/api/', {
+            params: {
+                size: size,
+                bold: 'true',
+                name: name
+            },
+            responseType: 'arraybuffer'
+        }).then((response) => {
+            const buffer = Buffer.from(response.data, 'binary').toString('base64');
+            setAvatar(`data:${response.headers['content-type'].toLowerCase()};base64,${buffer}`);
+        });
+    };
 
     return (
         <img className={className ? className : "rounded-circle" + (border ? " border border-1" : "")} width={size} height={size} src={avatar} alt={ name } />
