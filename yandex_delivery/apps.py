ecfecf14909bf3fbc74309fbead688686f15a7e2
Andrey Novikov <novikov@gmail.com>
2022-12-21 12:12:36 +0300

    Yandex delivery

---
+++ yandex_delivery/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class YandexDeliveryAppConfig(AppConfig):
+    name = 'yandex_delivery'
+    verbose_name = 'Яндекс.Доставка'
+
+    # def ready(self):
+    #     import beru.signals
