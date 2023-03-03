f2c208369a072d2d9336bcf458d580edba49e5b1
Andrey Novikov <novikov@gmail.com>
2023-03-03 21:13:33 +0300

    New react development approach

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 module.exports = {
     reactStrictMode: true,
     trailingSlash: true,
-    distDir: process.env.BUILD_DIR || '.next',
-    async rewrites() {
-        return [
-            {
-                source: '/api/v0/:path*/',
-                destination: 'http://127.0.0.1:8000/api/v0/:path*/' // Proxy to Backend
-            }
-        ]
-    }
+    output: 'standalone',
+    distDir: process.env.BUILD_DIR || '.next'
 }
