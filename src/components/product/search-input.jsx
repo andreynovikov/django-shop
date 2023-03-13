7bf6bb8181522cb360cc2ea19790238e7c038813
Andrey Novikov <novikov@gmail.com>
2023-03-13 16:45:03 +0300

    Search typeahead

--- src/components/product/search-input.jsx
+++ src/components/product/search-input.jsx
@@ -1,22 +1,15 @@
-import { useState, useEffect, useMemo, useDeferredValue } from 'react';
-import { useRouter } from 'next/router';
+import { useState, useMemo, useDeferredValue } from 'react';
 import { useQuery } from 'react-query';
+
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 
 import { productKeys, loadProductSuggestions } from '@/lib/queries';
 
-export default function ProductSearchInput() {
+export default function ProductSearchInput({ mobile }) {
     const [searchText, setSearchText] = useState('');
-    const [focused, setFocused] = useState(false);
     const deferredSearchText = useDeferredValue(searchText);
 
-    const router = useRouter();
-
-    useEffect(() => {
-        if (router.query.text !== undefined)
-            setSearchText(router.query.text);
-    }, [router.query.text]);
-
-    const { data: titles } = useQuery(
+    const { data: titles, isLoading } = useQuery(
         productKeys.suggestions(deferredSearchText),
         () => loadProductSuggestions(deferredSearchText),
         {
@@ -25,41 +18,47 @@ export default function ProductSearchInput() {
         }
     );
 
-    const suggest = (text) => {
-        setSearchText(text);
-    };
-
-    const handleBlur = () => {
-        setTimeout(() => { // otherwise click on suggestion is dismissed
-            setFocused(false);
-        }, 100);
-    };
-
-    const suggestions = useMemo(() =>
-        titles && titles.count > 1 && titles.results.map((title) => (
-            <a className="dropdown-item" key={title} onClick={() => suggest(title)} style={{ cursor: "pointer" }}>{ title }</a>
-        )),
-        [titles]
-    );
+    const options = useMemo(() => (
+        titles && titles.count > 0 && titles.results.filter((x, i, a) => a.indexOf(x) == i) || []
+    ), [titles]);
 
     return (
-        <div className="position-relative w-100 mx-4">
-            <form className="input-group d-none d-lg-flex" action="/search/">
-                <input
-                    className="form-control product-search rounded-end pe-5"
-                    name="text"
-                    type="text"
-                    placeholder="Поиск товаров"
-                    autoComplete="off"
-                    value={searchText}
-                    onChange={(e) => setSearchText(e.target.value)}
-                    onFocus={() => setFocused(true)}
-                    onBlur={handleBlur} />
-                <i className="ci-search position-absolute top-50 end-0 translate-middle-y text-muted fs-base me-3" />
-            </form>
-            <div className={"dropdown-menu" + (focused && searchText.length > 2 && titles && titles.count > 1 ? " d-block" : "")}>
-                { suggestions }
-            </div>
-        </div>
+        <form className="w-100" action="/search/">
+            <AsyncTypeahead
+                id={"product-search-input" + (mobile ? "-mobile" : "")}
+                className="w-100"
+                filterBy={() => true}
+                isLoading={isLoading}
+                minLength={3}
+                onSearch={(query) => setSearchText(query)}
+                options={options}
+                placeholder="Поиск товаров"
+                promptText="Введите строку для поиска..."
+                searchText="Ищем..."
+                emptyLabel="Ничего не найдено"
+                renderInput={({ inputRef, referenceElementRef, ...inputProps }) => (
+                    <div className="input-group">
+                        { mobile && <i className="ci-search position-absolute top-50 start-0 translate-middle-y text-muted fs-base ms-3" /> }
+                        <input
+                            {...inputProps}
+                            ref={(input) => {
+                                inputRef(input);
+                                referenceElementRef(input);
+                            }}
+                            onKeyUp={(e) => {
+                                if (e.key === "Enter") e.currentTarget.form.submit();
+                            }}
+                            className={"form-control pe-5 " + (mobile ? "rounded-start" : "rounded-end")}
+                            name="text"
+                        />
+                        { !mobile && <i className="ci-search position-absolute top-50 end-0 translate-middle-y text-muted fs-base me-3" /> }
+                    </div>
+                )}
+            />
+        </form>
     )
 }
+
+ProductSearchInput.defaultProps = {
+    mobile: false
+};
