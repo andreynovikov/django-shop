103992ad0fa3904a12002ed2aadfbb6f3c7f6958
Andrey Novikov <novikov@gmail.com>
2023-03-17 10:39:38 +0300

    Switch to React

--- src/components/user/sign-in-modal.jsx
+++ src/components/user/sign-in-modal.jsx
@@ -1,79 +1,69 @@
-import { useState, useEffect, forwardRef, useImperativeHandle, useRef } from 'react';
+import { useState, forwardRef, useImperativeHandle, useRef } from 'react';
+
+import Modal from 'react-bootstrap/Modal';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
 
 import LoginForm from '@/components/login-form';
 import RegistrationForm from '@/components/user/registration-form';
 
 export default forwardRef(function SignInModal({ctx: modalCtx}, ref) {
+    const [show, setShow] = useState(false);
+    const [key, setKey] = useState('signin');
     const [ctx, setCtx] = useState('login');
     const [phone, setPhone] = useState('');
 
     const modalRef = useRef();
-    const tabRef = useRef();
-
-    useEffect(() => {
-        if (phone !== '') {
-            const tabEle = tabRef.current;
-            const bsTab = bootstrap.Tab.getOrCreateInstance(tabEle);
-            bsTab.show();
-        }
-    }, [phone]);
 
     const showModal = () => {
-        const modalEle = modalRef.current;
-        const bsModal = new bootstrap.Modal(modalEle, {
-            backdrop: 'static',
-            keyboard: false
-        });
-        bsModal.show();
+        setShow(true);
     }
 
     const hideModal = () => {
-        const modalEle = modalRef.current;
-        const bsModal= bootstrap.Modal.getInstance(modalEle);
-        bsModal.hide();
+        setShow(false);
     }
 
     const onRegistrationComplete = (phone) => {
         setCtx('reg');
         setPhone(phone);
+        setKey('signin');
     };
 
-    useImperativeHandle(ref, () => ({
+    useImperativeHandle(ref, () => ({ // TODO: refactor?
         showModal,
         hideModal
     }));
 
     return (
-        <div className="modal fade" ref={modalRef} tabIndex="-1" role="dialog">
-            <div className="modal-dialog modal-dialog-centered" role="document">
-                <div className="modal-content">
-                    <div className="modal-header bg-secondary">
-                        <ul className="nav nav-tabs card-header-tabs" role="tablist">
-                            <li className="nav-item">
-                                <a className="nav-link fw-medium active" href={`#${modalCtx}-signin-tab`} data-bs-toggle="tab" role="tab" aria-selected="true" ref={tabRef}>
-                                    <i className="ci-unlocked me-2 mt-n1" />
-                                    Вход
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link fw-medium" href={`#${modalCtx}-signup-tab`} data-bs-toggle="tab" role="tab" aria-selected="false">
-                                    <i className="ci-user me-2 mt-n1" />
-                                    Регистрация
-                                </a>
-                            </li>
-                        </ul>
-                        <button className="btn-close" type="button" data-bs-dismiss="modal" onClick={hideModal} aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body tab-content py-4">
-                        <div className="tab-pane fade show active" id={`${modalCtx}-signin-tab`}>
+        <Modal ref={modalRef} show={show} onHide={hideModal} centered>
+            <Tab.Container id={`${modalCtx}-tabs`} activeKey={key} onSelect={(k) => setKey(k)}>
+                <Modal.Header closeButton>
+                    <Nav variant="tabs" className="card-header-tabs">
+                        <Nav.Item>
+                            <Nav.Link eventKey="signin" className="fw-medium">
+                                <i className="ci-unlocked me-2 mt-n1" />
+                                Вход
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="signup" className="fw-medium">
+                                <i className="ci-user me-2 mt-n1" />
+                                Регистрация
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Modal.Header>
+                <Modal.Body>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="signin">
                             <LoginForm embedded={modalCtx} ctx={ctx} phone={phone} hideModal={hideModal} />
-                        </div>
-                        <div className="tab-pane fade" id={`${modalCtx}-signup-tab`}>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="signup">
                             <RegistrationForm embedded={modalCtx} onComplete={onRegistrationComplete} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        </Tab.Pane>
+                    </Tab.Content>
+                </Modal.Body>
+            </Tab.Container>
+        </Modal>
     )
 });
