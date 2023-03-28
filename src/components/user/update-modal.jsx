5cff02eabc6979c851e294503af735624e4e03e9
Andrey Novikov <novikov@gmail.com>
2023-03-28 11:47:44 +0300

    Switch to React

--- src/components/user/update-modal.jsx
+++ src/components/user/update-modal.jsx
@@ -1,26 +1,22 @@
 import { useState, forwardRef, useImperativeHandle, createRef, useRef } from 'react';
 
+import Modal from 'react-bootstrap/Modal';
+
 import UpdateForm from '@/components/user/update-form';
 
 export default forwardRef(function UserUpdateModal(props, ref) {
+    const [show, setShow] = useState(false);
     const [formReady, setFormReady] = useState(false);
 
     const formRef = createRef();
     const modalRef = useRef();
 
     const showModal = () => {
-        const modalEle = modalRef.current;
-        const bsModal = new bootstrap.Modal(modalEle, {
-            backdrop: 'static',
-            keyboard: false
-        });
-        bsModal.show();
+        setShow(true);
     }
 
     const hideModal = () => {
-        const modalEle = modalRef.current;
-        const bsModal= bootstrap.Modal.getInstance(modalEle);
-        bsModal.hide();
+        setShow(false);
     }
 
     useImperativeHandle(ref, () => ({
@@ -29,20 +25,16 @@ export default forwardRef(function UserUpdateModal(props, ref) {
     }));
 
     return (
-        <div className="modal fade" ref={modalRef} tabIndex="-1" role="dialog">
-            <div className="modal-dialog modal-dialog-centered" role="document">
-                <div className="modal-content">
-                    <div className="modal-body">
-                        <UpdateForm embedded ref={formRef} onReady={() => setFormReady(true)} onUpdated={hideModal} />
-                    </div>
-                    { formReady && (
-                        <div className="modal-footer">
-                            <button type="button" class="btn btn-secondary btn-sm" onClick={hideModal}>Закрыть</button>
-                            <button type="submit" class="btn btn-primary btn-sm" onClick={() => formRef.current.submit()}>Сохранить</button>
-                        </div>
-                    )}
-                </div>
-            </div>
-        </div>
+        <Modal ref={modalRef} show={show} onHide={hideModal} centered backdrop="static">
+            <Modal.Body>
+                <UpdateForm embedded ref={formRef} onReady={() => setFormReady(true)} onUpdated={hideModal} />
+            </Modal.Body>
+            { formReady && (
+                <Modal.Footer>
+                    <button type="button" class="btn btn-secondary btn-sm" onClick={hideModal}>Закрыть</button>
+                    <button type="submit" class="btn btn-primary btn-sm" onClick={() => formRef.current.submit()}>Сохранить</button>
+                </Modal.Footer>
+            )}
+        </Modal>
     )
 });
