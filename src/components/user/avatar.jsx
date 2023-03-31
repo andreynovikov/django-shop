78fd0623f0325bfa7bbb7d25d07e3cf14889373b
Andrey Novikov <novikov@gmail.com>
2023-03-31 15:52:47 +0300

    Optimize avatar loading

--- src/components/user/avatar.jsx
+++ src/components/user/avatar.jsx
@@ -1,41 +1,42 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 import axios from 'axios';
 
-export default function UserAvatar({gravatar, name, size, border}) {
-    const [avatar, setAvatar] = useState('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=');
-
-    useEffect(() => {
-        const d = name.startsWith('+7') ? 'mp' : '404';
-        axios.get(gravatar + '&d=' + d, {
+async function loadAvatar(gravatar, name, size) {
+    const d = name.startsWith('+7') ? 'mp' : '404';
+    return axios.get(gravatar + '&d=' + d, {
+        responseType: 'arraybuffer'
+    }).then((response) => {
+        // const buffer = URL.createObjectURL(response.data); - with responseType: 'blob'
+        const buffer = Buffer.from(response.data, 'binary').toString('base64');
+        return `data:${response.headers['content-type'].toLowerCase()};base64,${buffer}`;
+    }).catch(() => {
+        return axios.get('https://ui-avatars.com/api/', {
+            params: {
+                size: size,
+                bold: 'true',
+                color: '4e54c8',
+                background: 'fff',
+                name: name
+            },
             responseType: 'arraybuffer'
         }).then((response) => {
-            // const buffer = URL.createObjectURL(response.data); - with responseType: 'blob'
             const buffer = Buffer.from(response.data, 'binary').toString('base64');
-            setAvatar(`data:${response.headers['content-type'].toLowerCase()};base64,${buffer}`);
-        }).catch((error) => {
-            axios.get('https://ui-avatars.com/api/', {
-                params: {
-                    size: size,
-                    bold: 'true',
-                    color: '4e54c8',
-                    background: 'fff',
-                    name: name
-                },
-                responseType: 'arraybuffer'
-            }).then((response) => {
-                const buffer = Buffer.from(response.data, 'binary').toString('base64');
-                setAvatar(`data:${response.headers['content-type'].toLowerCase()};base64,${buffer}`);
-            });
+            return `data:${response.headers['content-type'].toLowerCase()};base64,${buffer}`;
         });
-    }, [gravatar, name, size]);
+    });
+}
 
+export default function UserAvatar({gravatar, name, size=50, border=false}) {
+    const { data: avatar } = useQuery({
+        queryKey: ['avatars', { gravatar, name, size }],
+        queryFn: () => loadAvatar(gravatar, name, size),
+        placeholderData: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=',
+        staleTime: Infinity
+    });
+
+    /* eslint-disable @next/next/no-img-element */
     return (
         <img className={"rounded-circle" + (border ? " border border-1" : "")} width={size} height={size} src={avatar} alt={ name } />
     )
 }
-
-UserAvatar.defaultProps = {
-    size: 50,
-    border: false
-};
