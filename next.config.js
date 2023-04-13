a5f1eb518b359975196a2de796b3227d6a163a6a
Andrey Novikov <novikov@gmail.com>
2023-04-13 10:24:18 +0300

    Deploy via docker

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
     reactStrictMode: true,
     trailingSlash: true,
     output: 'standalone',
-    distDir: process.env.BUILD_DIR || '.next',
     sassOptions: {
         includePaths: [path.join(__dirname, 'src/vendor/sell/scss')],
     }
