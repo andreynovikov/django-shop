2acb1530a3f3f3f6f0a536f45c1e5c87493a4b7e
Andrey Novikov <novikov@gmail.com>
2023-04-14 09:52:00 +0300

    Smooth scroll

--- src/components/product/rating.jsx
+++ src/components/product/rating.jsx
@@ -5,7 +5,7 @@ import ReviewRating from '@/components/review/rating';
 import rupluralize from '@/lib/rupluralize';
 import { reviewKeys, getProductRating } from '@/lib/queries';
 
-export default function ProductRating({product}) {
+export default function ProductRating({product, anchor}) {
     const { data: average, isSuccess } = useQuery(
         reviewKeys.rating(product),
         () => getProductRating(product)
@@ -14,8 +14,19 @@ export default function ProductRating({product}) {
     if (!isSuccess || average.count === 0)
         return null;
 
+    const handleScroll = () => {
+        const el = document.getElementById(anchor);
+        if (el !== undefined)
+            el.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+    };
+
+    const props = anchor !== undefined ? {onClick: handleScroll, style: { cursor: 'pointer'}} : {};
+
     return (
-        <div>
+        <div {...props}>
             <ReviewRating value={ average.value } />
             <span className="d-inline-block fs-sm text-white opacity-70 align-middle mt-1 ms-1">
                 { average.count } { rupluralize(average.count, ['обзор', 'обзора', 'обзоров']) }
