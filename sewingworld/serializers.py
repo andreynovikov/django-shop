3ea0970db7e9c02610988aeabb4e0b5436646825
Andrey Novikov <novikov@gmail.com>
2023-04-14 13:50:09 +0300

    Fix product thumbnails and images ratio

--- sewingworld/serializers.py
+++ sewingworld/serializers.py
@@ -224,10 +224,10 @@ class ProductListSerializer(NonNullModelSerializer):
             return None
         filepath = obj.image_prefix + '.jpg'
         if not default_storage.exists(filepath):
-            filepath = obj.image_prefix + '.s.jpg'  # special case for variants (dor-tak)
+            filepath = obj.image_prefix + '.s.jpg'  # special case for variants
         if default_storage.exists(filepath):
             thumbnail_size = '{}x{}'.format(self.context.get('product_thumbnail_size'), self.context.get('product_thumbnail_size'))
-            image = get_thumbnail(filepath, thumbnail_size, padding=True)
+            image = get_thumbnail(filepath, thumbnail_size, padding=True, upscale=False)
             return {
                 'url': image.url,
                 'width': image.width,
