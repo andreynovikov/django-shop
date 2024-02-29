0a970e63fdccd37bf4a0961be876d7237ff039a6
Andrey Novikov <novikov@gmail.com>
2024-02-29 15:42:08 +0300

    Add default seller setting

--- sewingworld/forms.py
+++ sewingworld/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from djconfig.forms import ConfigForm
 
+from shop.models import Contractor
+
 
 class SWConfigForm(ConfigForm):
     sw_sms_provider = forms.ChoiceField(label='Смс провайдер', initial='sms_uslugi', choices=[('sms_uslugi', 'смс-услуги'), ('smsru', 'sms.ru')])
@@ -14,6 +16,8 @@ class SWConfigForm(ConfigForm):
     sw_yd_campaign = forms.CharField(label='Идентификатор кампании Яндекс.Доставка')
     sw_yd_sender = forms.CharField(label='Идентификатор магазина Яндекс.Доставка')
     sw_yd_token = forms.CharField(label='OAuth-токен магазина Яндекс.Доставка')
+
+    sw_default_seller = forms.ModelChoiceField(label='Продавец по-умолчанию', initial=None, queryset=Contractor.objects.filter(is_seller=True))
 
     sw_modulkassa_login = forms.CharField(label='Логин МодульКасса')
     sw_modulkassa_password = forms.CharField(label='Пароль МодульКасса')
