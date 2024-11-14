6e935c607b0d17ae8426ad4af21f1dc5acfa4d05
Andrey Novikov <novikov@gmail.com>
2024-11-14 12:42:38 +0300

    Add store filters to admin

--- shop/admin/__init__.py
+++ shop/admin/__init__.py
@@ -94,8 +94,8 @@ class StoreImageInline(SortableInlineAdminMixin, admin.TabularInline):
 class StoreAdmin(admin.ModelAdmin):
     list_display = ['city', 'address', 'name', 'supplier', 'enabled', 'latitude', 'longitude']
     list_display_links = ['address', 'name']
-    list_filter = [('city', RelatedDropdownFilter), 'enabled', 'publish']
-    search_fields = ['name', 'address', 'address2']
+    list_filter = [('city', RelatedDropdownFilter), 'enabled', 'publish', 'marketplace', 'lottery']
+    search_fields = ['name', 'address', 'address2', 'city__name']
     ordering = ['city', 'address']
     inlines = [StoreImageInline]
 
