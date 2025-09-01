0c51109a4770fffb5ea1b7a5c2860ec43b084fdb
Andrey Novikov <novikov@gmail.com>
2025-09-01 23:24:02 +0300

    Refactor top categories menu

--- src/components/top-categories-card.jsx
+++ src/components/top-categories-card.jsx
@@ -1,56 +1,53 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link';
+import Link from 'next/link'
 
 export default function TopCategoriesCard() {
     return (
-        <div className="card border-0 box-shadow-lg">
+        <div className="card mx-5 mx-xl-0 border-0 box-shadow-lg">
             <div className="card-body px-3 pb-0">
-                <div className="row g-0 justify-content-center">
-                    <div className="col-6 col-md-4 col-lg-2 mb-grid-gutter text-center">
+                <div className="row g-0 g-lg-3 justify-content-around">
+                    <div className="d-none d-lg-block col-lg-auto mb-grid-gutter text-center">
                         <Link href="/catalog/promo/">
                             <img src="/i/categories/sale.svg" alt="Специальные предложения" height="75" width="75" />
-                            <div>Специальные предложения</div>
+                            <div>Специальные<br/>предложения</div>
                         </Link>
                     </div>
-                    <div className="col-6 col-md-4 col-lg-2 mb-grid-gutter text-center">
+                    <div className="col-6 col-md-4 col-lg-auto mb-grid-gutter text-center">
                         <Link href="/catalog/sewing_machines/">
                             <img src="/i/categories/compsewing.svg" alt="Швейные машины" height="75" width="75" />
-                            <div>Швейные машины</div>
+                            <div>Швейные<br/>машины</div>
                         </Link>
                     </div>
-                    <div className="col-6 col-md-4 col-lg-2 mb-grid-gutter text-center">
+                    <div className="col-6 col-md-4 col-lg-auto mb-grid-gutter text-center">
                         <Link href="/catalog/embroidery_machines/">
                             <img src="/i/categories/embroidery.svg" alt="Швейно-вышивальные машины" height="75" width="94" />
-                            <div>Вышивальные машины</div>
+                            <div>Вышивальные<br/>машины</div>
                         </Link>
                     </div>
-                    <div className="col-6 col-md-4 col-lg-2 mb-grid-gutter text-center">
+                    <div className="col-6 col-md-4 col-lg-auto mb-grid-gutter text-center">
                         <Link href="/catalog/sergers/">
                             <img src="/i/categories/overlock.svg" alt="Оверлоки, коверлоки и распошивальные машины" height="75" width="75" />
-                            <div>Оверлоки и коверлоки</div>
+                            <div>Оверлоки и<br/>коверлоки</div>
                         </Link>
                     </div>
-                    <div className="col-6 col-md-4 col-lg-2 mb-grid-gutter text-center">
+                    <div className="col-6 col-md-4 col-lg-auto mb-grid-gutter text-center">
                         <Link href="/catalog/knitting_machines/">
                             <img src="/i/categories/knitting.svg" alt="Вязальные машины" height="75" width="94" />
-                            <div>Вязальные машины</div>
+                            <div>Вязальные<br/>машины</div>
                         </Link>
                     </div>
-                    <div className="col-6 col-md-4 col-lg-2 mb-grid-gutter text-center">
-                        <Link href="/catalog/accessories/">
-                            <img src="/i/categories/accessories.svg" alt="Аксессуары" height="75" width="75" />
-                            <div>Аксессуары</div>
-                        </Link>
-                    </div>
-                    { /*
-                    <div className="col-6 col-md-4 col-lg-2 mb-grid-gutter text-center">
+                    <div className="col-6 col-md-4 col-lg-auto mb-grid-gutter text-center">
                         <Link href="/catalog/accessories/threads/">
                             <img src="/i/categories/threads.svg" alt="Нитки" height="75" width="75" />
                             <div>Нитки</div>
                         </Link>
                     </div>
-                      */
-                    }
+                    <div className="col-6 col-md-4 col-lg-auto mb-grid-gutter text-center">
+                        <Link href="/catalog/accessories/">
+                            <img src="/i/categories/accessories.svg" alt="Аксессуары" height="75" width="75" />
+                            <div>Аксессуары</div>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
