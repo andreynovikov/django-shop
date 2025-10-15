340a397edc2c96b20b302afd08631badfcb89bf1
Andrey Novikov <novikov@gmail.com>
2025-10-15 20:00:18 +0300

    Error pages

---
+++ src/pages/500.jsx
@@ -0,0 +1,17 @@
+import BaseLayout from '@/components/layout/base'
+
+export default function Custom404() {
+  return <div className="m-5 text-center">
+    <div className="fs-2">К сожалению, что-то пошло не так</div>
+    <div className="lead mt-1">Мы постараемся исправить это как можно скорее</div>
+    <div className="mt-5 text-secondary" style={{fontSize: "10rem"}}>500</div>
+  </div>
+}
+
+Custom404.getLayout = function getLayout(page) {
+    return (
+        <BaseLayout title="Ошибка сервера">
+            {page}
+        </BaseLayout>
+    )
+}
