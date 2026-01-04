ae96607051cc0a3a257144733d79163f6440d081
Andrey Novikov <novikov@gmail.com>
2026-01-04 18:10:43 +0300

    Page revalidation

---
+++ src/pages/api/revalidate.js
@@ -0,0 +1,35 @@
+export default async function handler(req, res) {
+  console.log(req.body)
+  if (req.body?.secret !== process.env.REVALIDATION_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  if (!!!req.body.model || !!!req.body.pk) {
+    return res.status(400).json({ message: 'Parameters missing' })
+  }
+
+  try {
+    switch (req.body.model) {
+      case 'category':
+        await res.revalidate(`/catalog/${req.body.path}/`)
+        break
+      case 'product':
+        await res.revalidate(`/products/${req.body.code}/`)
+        break
+      case 'page':
+        await res.revalidate(`/pages${req.body.uri}`)
+        break
+      case 'news':
+        await res.revalidate('/news/')
+        break
+      default:
+        return res.json({ revalidated: false })
+    }
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    console.error(err)
+    return res.status(500).send('Revalidation error')
+  }
+}
