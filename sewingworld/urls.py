f8a03824728dc10e184452a63d9529280875fece
Andrey Novikov <novikov@gmail.com>
2026-06-22 22:48:52 +0300

    Remove sitemaps

--- sewingworld/urls.py
+++ sewingworld/urls.py
@@ -1,35 +1,15 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import include, path
 # from django.contrib import admin
-from django.contrib.sitemaps.views import index, sitemap
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.decorators.cache import cache_page
 
-from two_factor.urls import urlpatterns as tf_urls
-
-from forum.sitemaps import ThreadSitemap
 from rest_framework.routers import DefaultRouter
 
 from reviews.api import ReviewViewSet
 from forum.api import TopicViewSet, ThreadViewSet
 
-from shop.models.integration import Integration
-
 from . import api
 from . import views
-from .sitemaps import StaticViewSitemap, ProductSitemap, CategorySitemap, SalesActionSitemap, StoreSitemap, FlatPageSitemap
 
-
-sitemaps = {
-    'static': StaticViewSitemap,
-    'products': ProductSitemap,
-    'categories': CategorySitemap,
-    'sales': SalesActionSitemap,
-    'stores': StoreSitemap,
-    'pages': FlatPageSitemap,
-    'forum': ThreadSitemap
-}
 
 router = DefaultRouter()
 router.register(r'sites', api.SiteProfileViewSet, basename='site')
@@ -61,21 +41,6 @@ urlpatterns = [
     path('api/v0/blog/', include('blog.urls')),
     path('api/v0/csrf/', api.CsrfTokenView.as_view()),
     path('api/v0/warrantycard/<str:code>/', api.WarrantyCardView.as_view()),
-    # ex: /sitemap.xml
-    path('sitemap.xml', cache_page(60 * 60 * 24, cache='files')(index), {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.index'),
-    path('sitemap-<section>.xml', cache_page(60 * 60 * 24, cache='files')(sitemap), {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    # ex: /search.xml
-    path('search.xml', views.products, {'template': 'ya_search', 'filters': None}, name='ya_search_xml'),
-    # ex: /xml/search.xml
-    path('xml/search.xml', views.products, {'template': 'search', 'filters': None}, name='search_xml'),
-    # ex: /full.xml
-    path('full.xml', views.products, {'template': 'products', 'filters': None}, name='full'),
-    # ex: /products.xml
-    path('products.xml', views.products, {'template': 'products', 'filters': None}, name='products'),
-    # ex: /cc-prym.xml
-    path('cc-prym.xml', views.products, {'template': 'prym', 'filters': 'prym'}, name='cc-prym'),
-    # ex: /beru.xml
-    path('<slug:integration>.xml', views.products),
     # ex: /stock_avito.csv
     path('stock_avito.csv', views.stock, name='stock'),
     # /shop/*
