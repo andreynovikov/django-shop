8c68eb19a30e157ac97427507aa23a59ff1a26e9
Andrey Novikov <novikov@gmail.com>
2026-06-23 00:09:25 +0300

    Add typescript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import nextVitals from 'eslint-config-next/core-web-vitals'
- 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
-])
- 
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals"
+import nextTypescript from "eslint-config-next/typescript"
+import tanstackQuery from "@tanstack/eslint-plugin-query"
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  ...tanstackQuery.configs["flat/recommended"],
+  {
+    rules: {
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    ignores: [
+      "**/vendor/*",
+    ],
+  },
+  {
+    settings: {
+      react: { version: "19" },
+    },
+  },
+]
+
 export default eslintConfig
---
+++ next.config.ts
@@ -0,0 +1,46 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  output: 'standalone',
+  allowedDevOrigins: ['janome.club'],
+  images: {
+    remotePatterns: [
+      new URL('https://api.sewing-world.ru/media/**'),
+      new URL('https://www.sewing-world.ru/media/**'),
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/cart',
+        headers: [{ key: 'X-Robots-Tag', value: 'noindex' }],
+      },
+      {
+        source: '/compare',
+        headers: [{ key: 'X-Robots-Tag', value: 'noindex' }],
+      },
+      {
+        source: '/confirmation',
+        headers: [{ key: 'X-Robots-Tag', value: 'noindex' }],
+      },
+      {
+        source: '/search',
+        headers: [{ key: 'X-Robots-Tag', value: 'noindex' }],
+      },
+      {
+        source: '/user/:path*',
+        headers: [{ key: 'X-Robots-Tag', value: 'noindex' }],
+      },
+    ]
+  },
+  sassOptions: {
+    silenceDeprecations: ['import', 'if-function']
+  },
+  logging: {
+    browserToTerminal: true,
+  },
+}
+
+export default nextConfig;
