b08138c1ab445df3d0dc00e9812d4a7602db6f58
Andrey Novikov <novikov@gmail.com>
2026-07-31 11:08:01 +0300

    Favicons

--- src/components/layout/favicons.jsx
+++ src/components/layout/favicons.jsx
@@ -6,7 +6,7 @@ export default function Favicons() {
     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
     <link rel="icon" type="image/png" sizes="192x192" href="/favicons/favicon-192x192.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/favicons/favicon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/favicon-180x180.png" />
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
     <link rel="manifest" href="/manifest.json" />
   </>
