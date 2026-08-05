d02c54db9fde78785e2d0008309cf52441135574
Andrey Novikov <novikov@gmail.com>
2026-08-06 00:47:27 +0300

    Optimize sitemap rendering

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -60,7 +60,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     in_category: categories.map(category => category.id),
     variations: '',
   }
-  const pageSize = 1000
+  const pageSize = 100
   const order = 'id'
   let currentPage = 1
 
